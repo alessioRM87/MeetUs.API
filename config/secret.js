22aaba6a32b8ad73c2abdbab9177e105f54759e6
alessioRM87 <alessio.iannella@gmail.com>
2018-01-30 12:56:26 -0500

    // FIRST COMMIT
    Imported previous version of the api

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+    db_cloud:'mongodb://meetupadmin:meetupadmin@ds127564.mlab.com:27564/gps_locator_db',
+    db_local:'mongodb://localhost:27017/gps_locator_db',
+    db_secret:'mongodb_secret'
+
+}
+//meetupadmin         gps_admin
